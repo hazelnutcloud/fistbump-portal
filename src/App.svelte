79b9817abc743ed4f96cce61fc7ced8c159e6b7a
hazelnutcloud <ha5anmukhli50@gmail.com>
2021-10-01 22:22:00 +0800

    added messages

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script>
-	import { ethers } from "ethers";
-	import { fly, } from "svelte/transition";
-	import contractABI from "../public/WavePortal.json";
-	import Popup from './Popup.svelte'
+	import { BigNumber, ethers } from "ethers";
+	import { fly } from "svelte/transition";
+	import contractABI from "../public/FistbumpPortal.json";
+	import Popup from "./Popup.svelte";
+	import FistbumpCard from "./FistbumpCard.svelte";
 
 	const { ethereum } = window;
-	const contractAddress = "0xCc154123859d39047FF13eB7D61CBCCCbDE415a7";
+	const contractAddress = "0x05E8CE6b0b765e5a0fA742A2Dd46475cEeac1245";
 	let loading = false;
 	let accounts;
-	let count = 0;
+	let count = null;
 	let errorDisplay;
 	let success = false;
+	let fistbumpsArray = [];
+	let message = "";
 
 	async function authorizeWallet() {
 		try {
@@ -31,15 +34,16 @@
 			loading = true;
 			const provider = new ethers.providers.Web3Provider(ethereum);
 			const signer = provider.getSigner();
-			const wavePortalContract = new ethers.Contract(
+			const fistbumpPortalContract = new ethers.Contract(
 				contractAddress,
 				contractABI.abi,
 				signer
 			);
-			const waveTxn = await wavePortalContract.wave();
-			await waveTxn.wait();
+			const fistbumpTxn = await fistbumpPortalContract.fistbump(message);
+			await fistbumpTxn.wait();
 			loading = false;
 			getTotalFistbumps();
+			getAllFistbumps();
 			success = true;
 		} catch (error) {
 			loading = false;
@@ -51,12 +55,28 @@
 	async function getTotalFistbumps() {
 		try {
 			const provider = new ethers.providers.Web3Provider(ethereum);
-			const wavePortalContract = new ethers.Contract(
+			const fistbumpPortalContract = new ethers.Contract(
 				contractAddress,
 				contractABI.abi,
 				provider
 			);
-			count = await wavePortalContract.getTotalWaves();
+			count = await fistbumpPortalContract.getTotalFistbumps();
+		} catch (error) {
+			console.log(error);
+			return;
+		}
+	}
+
+	async function getAllFistbumps() {
+		try {
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const fistbumpPortalContract = new ethers.Contract(
+				contractAddress,
+				contractABI.abi,
+				provider
+			);
+			const newArray = await fistbumpPortalContract.getAllFistbumps();
+			fistbumpsArray = [...newArray].reverse();
 		} catch (error) {
 			console.log(error);
 			return;
@@ -67,14 +87,19 @@
 		success = false;
 	}
 
-	getTotalFistbumps();
+	function refresh() {
+		getTotalFistbumps();
+		getAllFistbumps();
+	}
+
+	refresh();
 </script>
 
 <div class="container">
 	<h1>🗿 Greetings.</h1>
 	<h2>
-		I'm Hasan, I used to fly airplanes for a living.<br />Now all I do is code
-		💻.<br />Give me a 👊 if you love to code too.
+		I'm Hasan, I used to fly airplanes for a living.<br />Now all I do is
+		code 💻.<br />Give me a 👊 if you love to code too.
 	</h2>
 
 	{#if !ethereum}
@@ -82,28 +107,50 @@
 			><button>🦊 Get metamask</button></a
 		>
 	{:else if !accounts}
-		{#if loading}
-			<button disabled>⏳ Connecting metamask...</button>
-		{:else}
+		{#if !loading}
 			<button on:click={authorizeWallet}>🦊 Connect metamask</button>
+		{:else}
+			<button disabled>⏳ Connecting metamask...</button>
 		{/if}
-	{:else if loading}
-		<button disabled
-			>🤜💥🤛 Bumping fists... <span class="loading"
-				><i class="fab fa-ethereum" /></span
-			></button
-		>
 	{:else}
-		<button on:click={fistbump}>👊 Fistbump!</button>
+		<input
+			type="text"
+			placeholder="send me a message too if you'd like 😇"
+			bind:value={message}
+			disabled={!!loading}
+		/> <br />
+		{#if !loading}
+			<button on:click={fistbump}>👊 Fistbump!</button>
+		{:else}
+			<button disabled
+				>🤜💥🤛 Bumping fists... <span class="loading"
+					><i class="fab fa-ethereum" /></span
+				></button
+			>
+		{/if}
 	{/if}
 
-	{#key count}<p style="margin-bottom: 0;">
-			Total 👊's bumped: <span
-				in:fly={{ y: 20, duration: 800 }}
-				style="display: inline-block">{count}</span
-			>
-		</p>{/key}
-	<p class="refresh" on:click={() => getTotalFistbumps()}>
+	{#if count}
+		<div class="list-container">
+			{#each fistbumpsArray as fistbump}
+				<FistbumpCard {fistbump} />
+			{/each}
+		</div>
+	{/if}
+
+	{#if count}
+		{#key count}<p style="margin-bottom: 0;">
+				Total 👊's bumped: <span
+					in:fly={{ y: 20, duration: 800 }}
+					style="display: inline-block">{count}</span
+				>
+			</p>
+		{/key}
+	{:else}
+		<p>connect to metamask to see all 👊's bumped!</p>
+	{/if}
+
+	<p class="refresh noselect" on:click={() => refresh()}>
 		<i class="fas fa-sync-alt" /> refresh
 	</p>
 
@@ -119,13 +166,13 @@
 	{/if}
 </div>
 {#if success}
-<Popup handleClick={closePopup}/>
+	<Popup handleClick={closePopup} />
 {/if}
 
 <style>
 	button {
 		border-radius: 8px;
-		padding: 0.6em 1em;
+		padding: 0.5em 1em;
 	}
 	h2 {
 		font-weight: 500;
@@ -142,7 +189,25 @@
 		backdrop-filter: blur(3px);
 		-webkit-backdrop-filter: blur(3px);
 		border: 1px solid rgba(255, 255, 255, 0.3);
-		padding: 1em;
+		padding: 2em;
+	}
+	input {
+		width: 100%;
+		margin: 1em 0;
+		max-width: 400px;
+		border-radius: 8px;
+	}
+	.list-container {
+		height: 200px;
+		overflow-y: scroll;
+		/* From https://css.glass */
+		background: rgba(0, 0, 0, 0.6);
+		border-radius: 4px;
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(5px);
+		-webkit-backdrop-filter: blur(5px);
+		border: 1px solid rgba(0, 0, 0, 0.3);
+		margin-top: 1em;
 	}
 	.error {
 		color: red;
@@ -157,8 +222,16 @@
 	.loading {
 		animation: loading 2s;
 		animation-iteration-count: infinite;
-		/* animation-timing-function:cubic-bezier(0.6, -0.28, 0.735, 0.045); */
 		display: inline-block;
+	}
+	.noselect {
+		-webkit-touch-callout: none; /* iOS Safari */
+		-webkit-user-select: none; /* Safari */
+		-khtml-user-select: none; /* Konqueror HTML */
+		-moz-user-select: none; /* Old versions of Firefox */
+		-ms-user-select: none; /* Internet Explorer/Edge */
+		user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 	}
 	@keyframes loading {
 		50% {
---
+++ src/FistbumpCard.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let fistbump;
+</script>
+
+<div class="container">
+    <p><span class="bold">Sender:</span> {fistbump.sender}</p>
+    <p><span class="bold">Message:</span> {fistbump.message}</p>
+    <p><span class="bold">Time:</span> {new Date(fistbump.timestamp * 1000)}</p>
+</div>
+
+<style>
+    .bold {
+        font-weight: 700;
+    }
+    p {
+        color: #F1F1F1;
+    }
+    .container {
+        text-align: left;
+        padding: 0.2em 1em;
+        border-bottom: 1px solid black;
+    }
+</style>
